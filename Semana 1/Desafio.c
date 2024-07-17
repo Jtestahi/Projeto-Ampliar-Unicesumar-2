2b35e5873e6a7b92ba3a2a5c115b1a96a32a38bb
Jtestahi <joaovitortestahi6@gmail.com>
2024-07-17 20:43:30 -0300

    Update Desafio.c

--- Semana 1/Desafio.c
+++ Semana 1/Desafio.c
@@ -1,28 +1,24 @@
-/*
-    Escreva um algoritmo que leia o nome de um aluno e suas duas notas de matem�tica.
-    Ent�o, escreva na tela o nome e a m�dia do aluno.
-*/
-
 #include <stdio.h>
 
 int main() {
-    float nota1, nota2, media;
-    char full_name[50];
+    
+    float num1, num2, media;
+    char nome[50];
 
-    printf("Calcular media do Aluno.\n\n");
+    printf("Calcule a media dos alunos. \n\n");
 
-    printf("Digite o nome completo do aluno: ");
-    scanf("%[^\n]s", &full_name);
+    printf("Digite o nome do aluno: ");
+    scanf("%s", nome);
 
     printf("Digite a primeira nota: ");
-    scanf("%f", &nota1);
+    scanf("%f", &num1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", &nota2);
+    scanf("%f", &num2);
+    
+    media = (num1 + num2) / 2;
 
-    media = (nota1 + nota2)/2;
-
-    printf("A media de %s é: %.2f\n",full_name, media);
+    printf("O aluno %s tem a media de: %f\n", nome, media);
 
     return 0;
 }
