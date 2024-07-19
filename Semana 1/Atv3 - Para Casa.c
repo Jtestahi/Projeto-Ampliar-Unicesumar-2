2f2c98a40676e782bc1b3a3a9dcbaecbc76fb54b
Jtestahi <joaovitortestahi6@gmail.com>
2024-07-19 00:54:51 -0300

    Update Atv3 - Para Casa.c

--- Semana 1/Atv3 - Para Casa.c
+++ Semana 1/Atv3 - Para Casa.c
@@ -1,13 +1,22 @@
 /*
-    Escreva um programa que receba o sal·rio de um funcion·rio e
+    Escreva um programa que receba o sal√°rio de um funcion√°rio e
     calcule quanto ele precisa pagar de imposto de renda (aliquota 25%)
 */
 
-#include <stdio.h>
-
 int main() {
+    float salario, result, result2;
 
+    printf("Digite o valor do seu salario?: \n");
+    scanf("%f", &salario);
 
+    result = salario*25/100;
+
+    result2 = salario - result;
+
+    
+    printf("O imposto de (aliquota 25 por cento) sobre o seu salario e de: %.2f \n", result);
+
+    printf("O salario liquido e de: %.2f", result2);
     
     return 0;
 }
